ff7f2e4e95758b009291120ff2648a51601d7831
Kevin "Kenny" Niehage <kenny@syseleven.de>
2021-06-07 11:18:48 +0200

    updated version and copyright year

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-  # Shared-Secrets v0.27b0
+  # Shared-Secrets v0.28b0
   #
-  # Copyright (c) 2016-2020, SysEleven GmbH
+  # Copyright (c) 2016-2021, SysEleven GmbH
   # All rights reserved.
   #
   # This page allows you to share a secret through a secret sharing link.
